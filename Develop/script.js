3f6962e7188891d2dc06db446a0e7c73ae30f192
Robby <n.robinson@Robinsons-MBP.cable.rcn.com>
2021-12-12 17:23:36 -0500

    added all functions

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,30 @@
 // Assignment code here
 
-var passwordCriteria = [confirmLowerCase,]
+
+finalArray = [];
+function lowercaseGenerator () {
+  var result = " ";
+  var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  var passwordLength = password.length;
+  for (var i = 0; i< length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * passwordLength));
+  }
+  result.toLowerCase
+  return result;
+}
+console.log(lowercaseGenerator());
+
+function uppercaseGenerator() {
+  String.toUppercase()
+}
+
+function numericGenerator () {
+  Number()
+}
+
+//function specialGenerator () {
+  //String.
+//}
 
 function generatePassword() {
   var passwordLength = window.prompt("How many characters do you want your password to contain? (Choose a number between 8 and 128)")
@@ -14,24 +38,37 @@ function generatePassword() {
 
   var confirmLowerCase = window.confirm("Would you like to include lowercase characters?")
   if (confirmLowerCase === true) {
-    function generateLowerCase() {
-      return String.charAt(0).toUpperCase() + String.slice(1);
-    }
+    finalArray.push(lowercaseGenerator)
   }
   else {
-    
-
-
+    console.log("Thanks");
   }
 
-  var confirmUpperCase = window.confirm("would you like to include uppercase characters?")
-  if (confirmUpperCase = true) {
-    console.log ("Okay");
+  var confirmUpperCase = window.confirm("Would you like to include uppercase characters?")
+  if (confirmUpperCase === true) {
+    finalArray.push(uppercaseGenerator)
   }
     else {
       console.log("Thanks")
     }
+
+
+  var confirmNumeric = window.confirm("Would you like to include numeric characters?")
+  if (confirmNumeric === true) {
+    finalArray.push(numericGenerator)
+    }
+
+  //var confirmSpecialChar = window.confirm("Would you like to include special characters?")
+  //if (confirmSpecialChar === true) {
+    //finalArray.push(specialGenerator)
+  //}
+
   }
+
+  function randomNumber() {
+    finalArray
+  }
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
